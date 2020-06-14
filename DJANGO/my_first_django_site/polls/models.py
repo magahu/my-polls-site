16d98b01e98e42373a45fce5909f3940cb4417fa
Margarita <margaritagahu@gmail.com>
2020-06-14 17:16:39 -0500

    nav working

--- DJANGO/my_first_django_site/polls/models.py
+++ DJANGO/my_first_django_site/polls/models.py
@@ -17,7 +17,15 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    #votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+
+class Vote(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
